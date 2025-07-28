8ed0b86f0e12124a300922553f19133512f43b45
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-07-28 17:52:16 +0600

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://opshr.netlify.app"],
     credentials: true,
   })
 );
