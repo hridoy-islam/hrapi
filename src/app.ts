2e0336dee354b09afffba723186c2e0a61ae1144
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-08-13 19:38:22 +0600

    cors  update

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://opshr.netlify.app"],
+    origin: ["http://localhost:5173","https://opshr.netlify.app","https://hr.watneycollege.co.uk"],
     credentials: true,
     
   })
