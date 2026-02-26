2c6ba07341597e2dc82240f6d3e394cbb12923e4
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2026-02-26 15:01:20 +0600

    update leave model

--- src/app/modules/hr/leave/leave.model.ts
+++ src/app/modules/hr/leave/leave.model.ts
@@ -7,6 +7,7 @@ const LeaveSchema = new Schema(
   {
     holidayYear: { type: String, required: true }, // e.g., "2023-2024"
     userId: { type: Types.ObjectId, ref: "User", required: true },
+    companyId: { type: Types.ObjectId, ref: "User", required: true },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     reason: { type: String,  },
